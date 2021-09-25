30e7dcbf0a7ad203d13058985173ff9b774e4d5a
slowy07 <slowy.arfy@gmail.com>
2021-09-25 10:25:34 +0700

    add: basic hello world
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ basic/hello_world.cpp
@@ -0,0 +1,23 @@
+// iostream library adalah header yang berfungsi
+// untuk input dan ouput data
+#include <iostream>
+
+// using namespace std adalah namespace standar dari c++
+// yang dikgunakan untuk object dan variabel
+// yang terdapat pada standar library c++
+using namespace std;
+
+
+// merupakan tempat dimana semua kode akan di esekusi
+// dan dijalankan oleh compiler
+// setiap statement pada c++ wajib diakhiri dengan ; (titik koma)
+int main(){
+    
+    // cout yang kata lain dari "see out" yang digunakan sebagai
+    // operator output dengan menggunakan operator masukan (<<)
+    // seperti pada contoh kita akan memasukkan kata "bellshade"
+    cout<<"bellshade!";
+
+    // return 0 adalah akhir dari program yang diesekusi    
+    return 0;
+}
