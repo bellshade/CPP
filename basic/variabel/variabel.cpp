158ce5574a2db1334af88e61b919fd94a156c222
Call Vin <KAnggara75@gmail.com>
2021-10-02 16:13:26 +0700

    add: variabel example

---
+++ basic/variabel/variabel.cpp
@@ -0,0 +1,32 @@
+// Contoh penggunan variabel dengan tipe data integer dan float
+
+#include <iostream>
+
+int main() {
+  //Metode 1
+  //1. Melakukan deklarasi variabel dengan menetapkan nilainya pada baris terpisah
+  // Deklasari tipe data integer pada variabel (a, b dan c)
+  int a;
+  int b;
+  int c;
+
+  // Inisialisai nilai untuk setiap variabel
+  a = 2;
+  b = 3;
+  c = a + b; 
+
+  // Menampilkan hasil penjumlahan a + b
+  std::cout << a << " + " << b << " = " << c << '\n';
+
+  //Metode 2
+  //Melakukan deklarasi variabel dengan menetapkan nilainya pada baris yang sama
+  // Deklarasi variabel (d, e dan f) dan menetapkan nilainya dengan tipe data float (bilangan pecahan)
+  float d = 2.5f;
+  float e = 3.14f;
+  float f = d + e;
+
+  // Menampilkan hasil penjumlahan d + e
+  std::cout << d << " + " << e << " = " << f << '\n';
+
+  return 0;
+}
