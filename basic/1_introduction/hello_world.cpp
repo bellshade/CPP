1393b539dbe392935724e55e43e5c53d9d154d41
Arif Yunando <68750286+arifyunando@users.noreply.github.com>
2021-10-04 14:49:03 +0700

    fix: penjelasan fungsi dan typo

--- basic/1_introduction/hello_world.cpp
+++ basic/1_introduction/hello_world.cpp
@@ -1,23 +1,30 @@
-// iostream library adalah header yang berfungsi
-// untuk input dan ouput data
+// '#' adalah indikator perintah pre-prosesor
+// include adalah perintah untuk memasukan library ke source code
+// iostream adalah library standar bawaan C++
+// yang berfungsi untuk input dan ouput data
 #include <iostream>
 
-// using namespace std adalah namespace standar dari c++
-// yang dikgunakan untuk object dan variabel
-// yang terdapat pada standar library c++
+// using namespace adalah deklarasi penggunaan
+// pada satu lingkup fungsi atau dokumen C++
+// std adalah namespace yang digunakan C++ 
+// untuk menandakan objek dan variabel
+// yang terdapat pada seluruh standar library C++
+// termasuk iostream
 using namespace std;
 
 
-// merupakan tempat dimana semua kode akan di esekusi
-// dan dijalankan oleh compiler
+// fungsi main() di bawah adalah entry point yang
+// merupakan tempat dimana kode akan pertama kali 
+// dieksekusi dan dijalankan oleh compiler
 // setiap statement pada c++ wajib diakhiri dengan ; (titik koma)
 int main(){
     
-    // cout yang kata lain dari "see out" yang digunakan sebagai
-    // operator output dengan menggunakan operator masukan (<<)
-    // seperti pada contoh kita akan memasukkan kata "bellshade"
+    // cout yang merupakan singkatan dari "Character OUTput" digunakan sebagai
+    // perintah output dengan menggunakan operator left shift (<<)
+    // Pada contoh berikut, kita akan memasukkan kata "bellshade"
     cout<<"bellshade!";
 
-    // return 0 adalah akhir dari program yang diesekusi    
+    // return 0 adalah perintah untuk mengeluarkan nilai integer 0
+    // apabila program berhasil dijalankan tanpa error
     return 0;
 }
