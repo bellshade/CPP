43125d4745b4975b03920d635d34c3c1c8ac4e32
arfy slowy <slowy.arfy@gmail.com>
2021-10-05 07:04:58 +0700

    fix: menambahkan penjelasan dan memperbaiki indent

--- backtracking/minimax.cpp
+++ backtracking/minimax.cpp
@@ -1,8 +1,39 @@
+/**
+ * @file
+ * @brief 
+ * mengembalikan nilai terpanjang / dan terpendek
+ * algoritma minimax bisa dilihat disini
+ * https://en.wikipedia.org/wiki/Minimax
+ * @details
+ * minimax aadalah seubah aturan keuputsan
+ * yang digunakan dalam kecerdasan buatan, teori permainan
+ * statistik, dan teori keputusan dan
+ * untuk meminimalkan kemungkinan kerugian untuk beberapa
+ * skenario (kerugian maksimum)
+ * ketika berhadapan dengan keuntungan, ini disebut dengan maximin
+ * untuk memaksimalkan nilai keutungan minimum
+ * awalnya diformulasikan untuk teori permainan zero-sum dua pemain,
+ * yang mencakup kasus di mana pemain mengambil gerakan alternatif dan mereka
+ * juga melakukan gerakan simultan, itu juga tekah diperluas ke permain yang lebih
+ * kompleks dan pengambilan keputusan umum di hadapan ketidakpastian
+ */
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <array>
 namespace backtracking {
+    /**
+        * periksam nomo mana yang  maksimum / minimum dalam array
+        * @param depth kedalaman saat ini di dalam pohon permainan
+        * lebih singkat tentang pohon permainan
+        * https://en.wikipedia.org/wiki/Game_tree
+        * @param nodex_index indeks saat ini dalam array
+        * @param is_max jika index saat ini adalah angka terpanjang
+        * @param score menyimpan di dalam array
+        * @param height tinggi maksimum untuk pohon permainan
+        * @return max atau min dari angka
+        */
 template <size_t T>
 int minimax(int depth, int node_index, bool is_max,
             const std::array<int, T> &scores, double height) {
@@ -14,8 +45,9 @@ int minimax(int depth, int node_index, bool is_max,
     int v2 = minimax(depth + 1, node_index * 2 + 1, !is_max, scores, height);
 
     return is_max ? std::max(v1, v2) : std::min(v1, v2);
+    }
 }
-} 
+
 int main() {
     std::array<int, 8> scores = {90, 23, 6, 33, 21, 65, 123, 34423};
     double height = log2(scores.size());
