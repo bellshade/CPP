09aef36e580b1611543552140a2fd22835a40529
slowy07 <slowy.arfy@gmail.com>
2021-10-12 23:14:04 +0700

    feat: menambahkan tutorial break and continue
    close #11
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ basic/6_break_continue/cpp_break.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    // membuat perulangan dengan value i
+    // dari 0 sampai dengan 50
+    for(int i = 0; i < 50; i++){
+        // jika i adalah 20
+        if (i == 20){
+            // maka akan menghentikan perulangan
+            break;
+        }
+        // menampilkan i
+        // atau hasil perulangan
+        std::cout<<i<<"\n";
+    }
+    return 0;
+}
