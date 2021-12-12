60a457381dd8a931da12ee4165422698d3d3e561
slowy07 <slowy.arfy@gmail.com>
2021-12-12 23:34:26 +0700

    feat: count of set bits
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ algorithm/bit_manipulation/count_of_set_bits.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file count_of_set_bits.cpp
+ * @details Count of set bits
+ * diberikan angka integer. kemudian kalkulasi angka dari bits
+ */
+
+#include <cassert>
+#include <iostream>
+
+namespace bit_manipulation{
+    namespace count_of_set_bits{
+        std::uint64_t countSetbits(std::int64_t n){
+            int count = 0;
+            while (n != 0){
+                ++count;
+                n = (n & (n - 1));
+            }
+            return count;
+        }
+    }
+}
+
+static void test(){
+    assert(bit_manipulation::count_of_set_bits::countSetbits(4) == 1);
+}
+
+int main(){
+    test();
+    
+    return 0;
+}
