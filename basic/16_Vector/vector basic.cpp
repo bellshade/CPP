1e1dabd00b5042dae5a4a942a62d3e4bed66b8ba
Aldi Ananda Prasetya <58779191+AltQwerty25@users.noreply.github.com>
2022-10-05 14:27:40 +0700

    Update vector basic.cpp

--- basic/16_Vector/vector basic.cpp
+++ basic/16_Vector/vector basic.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 #include <vector>
 
-void loop(std::vector <int> parameter){
-    // Membaca nilai vector dari awal - akhir
-    for(int i = 0; i < parameter.size(); i++){
-        std::cout << parameter[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
 int main(int argc, char const *argv[])
 {
-    // Penjabaran: vector <tipe_data> nama_variabel
 
     // Deklarasi Vector
     std::vector <int> v;
@@ -20,16 +11,9 @@ int main(int argc, char const *argv[])
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
-
-    std::cout << "Nilai Awal: ";
-    // Memanggil Fungsi looping
-    loop(v);
     
-    // Index Vector 1 (2) akan di hapus
-    // dan digantikan posisinya dengan Index Vector 2 (3)
-    v.erase(v.begin() + 1, v.begin() + 2);
-    std::cout << "Nilai Baru: ";
-    loop(v);
+    std::cout << "Nilai Awal: ";
+    std::cout << v[0] << v[1] << v[2] << std::endl;
     
     return 0;
 }
