da542ea96b0cc9bc9994bace1f3edf3ef13698af
Aldi Ananda Prasetya <58779191+AltQwerty25@users.noreply.github.com>
2022-10-06 09:29:24 +0700

    Create basic_vector.cpp

---
+++ basic/16_Vector/basic_vector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+void loop(std::vector <int> parameter){
+    std::cout << "\nData Vector berisikan: ";
+    for(int i = 0; i < parameter.size(); i++){
+        std::cout << "[" << parameter[i] << "] ";
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    // Deklarasi Vector
+    std::vector <int> v;
+
+    // 1. push_back
+    // Menginisialisasikan Vector dengan angka = 1, 2, 3
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    loop(v);
+
+    // 2. pop_back
+    v.pop_back();
+    loop(v);
+
+    // 3. at
+    v.at(1) = 3;
+    loop(v);
+
+    // 4. clear
+    v.clear();
+    loop(v);
+    
+    return 0;
+}
