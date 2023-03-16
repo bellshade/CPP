ea07757b0cf1c3f35c800490125d4c6ed1ad946c
kedaci <kedaci@duck.com>
2023-03-16 22:52:31 +0800

    feat: menambahkan komentar

--- algorithm/sorting/snail_sort.cpp
+++ algorithm/sorting/snail_sort.cpp
@@ -6,35 +6,44 @@ using std::cout;
 using std::pair;
 using std::vector;
 
+// melakukan alias pada vector<vector<int>> menjadi Matrix
 using Matrix = vector<vector<int>>;
 
 vector<int> snail_sort(const Matrix& mat) {
+  // mengecek matrix mat
+  // jika kosong maka return vector kosong
   if (mat.empty() || mat.at(0).empty()) return {};
 
+  // mengontrol arah pointer
   auto redirect = [](pair<int, int> delta) -> pair<int, int> {
-    if (delta == pair{0, 1}) return {-1, 0};   // right to bottom
-    if (delta == pair{-1, 0}) return {0, -1};  // bottom to left
-    if (delta == pair{0, -1}) return {1, 0};   // left to top
-    return {0, 1};                             // go to right again
+    if (delta == pair{0, 1}) return {-1, 0};   // kanan ke bawah
+    if (delta == pair{-1, 0}) return {0, -1};  // bawah ke kiri
+    if (delta == pair{0, -1}) return {1, 0};   // kiri ke atas
+    return {0, 1};                             // atas ke kanan
   };
 
-  int n = mat.size();
-  int m = mat.at(0).size();
+  size_t n = mat.size();
 
-  Matrix marker(n, vector<int>(m));
-  vector snail = {mat.at(0).at(0)};
+  Matrix marker(n, vector<int>(n));  // matrix untuk mark
+  vector snail = {mat.at(0).at(0)};  // hasil dari sorting
 
-  pair pos = {0, 0};
-  pair delta = {0, 1};
+  pair pos = {0, 0};    // posisi saat ini (pointer)
+  pair delta = {0, 1};  // arah pointer sekarang
 
-  while (snail.size() < (n * m)) {
+  while (snail.size() < (n * n)) {
+    // melakukan binding pada variable yang dibutuhkan (C++17)
     auto [x, y] = pos;
     auto [dx, dy] = delta;
     auto [next_x, next_y] = pair{x + dx, y + dy};
 
+    // mengecek agar posisi (pointer) selanjut nya
+    // tidak keluar dari index matrix
     auto left_right = (next_x < 0 || next_x >= n);
     auto up_down = (next_y < 0 || next_y >= n);
 
+    // mengecek mark, jika 1 maka ubah arah pointer (delta)
+    // jika 0 masukan posisi selanjut nya ke hasil sorting (snail),
+    // mark posisi sekarang menjadi 1 dan ganti posisi sekarang (pos)
     if (left_right || up_down || marker.at(next_x).at(next_y)) {
       delta = redirect(delta);
     } else {
@@ -47,6 +56,7 @@ vector<int> snail_sort(const Matrix& mat) {
   return snail;
 }
 
+// Mencetak vector
 void print_vec(const vector<int>& vec) {
   cout << "{";
   for (auto x : vec) {
@@ -56,12 +66,10 @@ void print_vec(const vector<int>& vec) {
 }
 
 int main(void) {
+  // membuat 2 matrix untuk di test
   Matrix matrix1 = {
-      {1, 2, 3, 4, 5},
-      {6, 7, 8, 9, 10},
-      {11, 12, 13, 14, 15},
-      {16, 17, 18, 19, 20},
-      {21, 22, 23, 24, 25},
+      {1, 2, 3, 4, 5},      {6, 7, 8, 9, 10},     {11, 12, 13, 14, 15},
+      {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25},
   };
 
   Matrix matrix2 = {
@@ -70,9 +78,10 @@ int main(void) {
       {7, 8, 9},
   };
 
-  vector snail1 = snail_sort(matrix1);
-  vector snail2 = snail_sort(matrix2);
+  vector<int> snail1 = snail_sort(matrix1);
+  vector<int> snail2 = snail_sort(matrix2);
 
+  // cetak hasil sorting
   print_vec(snail1);
   print_vec(snail2);
 }
