2e29b979dbd521f732f87e498e87541d654f3fc3
Anggiramadyansyah <anggirrr31@gmail.com>
2023-04-23 00:56:30 +0700

    Update ganjil_genap.cpp

--- math/ganjil_genap.cpp
+++ math/ganjil_genap.cpp
@@ -6,14 +6,15 @@ int main(int argc, char *argv[]){
   std::cout<<"Masukan angka: ";
   std::cin>>input;
   
+  # Setiap karakter akan diperiksa untuk memastikan bahwa input harus berupa angka.
   for (const auto& itr : input){
     if (!(itr >= '0' && itr <= '9')){
       std::cout<<"Input harus angka!";
-      return 7;
+      return 7; # Nilai 7 biasanya menunjukkan adanya kesalahan dalam program.
     }
   }
   
-  int parseInt = stoi(input);
+  int parseInt = stoi(input); # Konversi string input menjadi integer dan menyimpannya ke variabel parseInt.
   if (parseInt % 2 == 0){
     std::cout<<"Ini adalah bilangan genap.";
   } else { 
