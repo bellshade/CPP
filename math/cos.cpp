a704b46c158df4a0f29567294c7663fb7c86f34c
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2023-07-16 22:15:10 +0700

    feat: menambahkan algoritma cosinus (#99)
    * feat: menambahkan algoritma cosinus
    
    ``Deskripsi``
    cosinus merupakan salah satu fungsi trigonometri
    dari sebuah titik. cosinus ini juga bisa dikatakan
    merupakan bilangan real dari bentuk panjang dari garis
    yang bergelombang dan juga bisa dimasukan salah satu di dalam
    pembahasan infinite series.
    kali ini saya menggunakan metode dari salah satu infitine series
    yaitu taylor series.
    
    [wikipedia](https://en.wikipedia.org/wiki/Sine_and_cosine)
    [algoritm_Source](https://people.math.sc.edu/girardi/m142/handouts/10sTaylorPolySeries.pdf)
    
    * feat: menambahkan algoritma cosinus
    
    ``Deskripsi``
    cosinus merupakan salah satu fungsi trigonometri
    dari sebuah titik. cosinus ini juga bisa dikatakan
    merupakan bilangan real dari bentuk panjang dari garis
    yang bergelombang dan juga bisa dimasukan salah satu di dalam
    pembahasan infinite series.
    kali ini saya menggunakan metode dari salah satu infitine series
    yaitu taylor series.
    
    [wikipedia](https://en.wikipedia.org/wiki/Sine_and_cosine)
    [algoritm_Source](https://people.math.sc.edu/girardi/m142/handouts/10sTaylorPolySeries.pdf)
    
    * feat: commit_msg.txt and binary

---
+++ math/cos.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+
+
+/**
+ * @file cos.cpp
+ * @brief fungsi untuk mengetahui Cos nilai output dengan
+ * menggunakan algoritma
+*/
+
+
+int factorial(int n){
+    if (n<0){
+        throw std::invalid_argument("Angka faktorial harus real");
+    }
+    else if (n==0 or n==1){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+}
+
+
+/**
+ * @brief fungsi untuk mengetahui nilai cosinus pada titik
+ * dengan menggunakan algoritma taylor series
+ * @param sudut merupakan titik input untuk mengetahui
+ *              nilai cos dari titik tersebut
+ * @param iterable merupakan jumlah perulangan perjumlah yang dilakukan
+ *                 default 4
+ * @return hasilnya berupa angka decimal
+*/
+
+
+template <typename T> double cosinus(T sudut,int iterable=4){
+    double result = 0.0;
+    for (int n = 0; n <= iterable; ++n){
+        double numerator = pow(-1,n) * pow(sudut,2*n);
+        double denumerator = factorial(2*n);
+        result =result + (numerator/denumerator); 
+    }
+    return result;
+}
+
+
+int main(){
+    // testing
+    std::vector<double> list_sudut ={M_PI,M_PI/2,M_PI * 3/4};
+    for(int i =0;i<list_sudut.size();++i){
+        std::cout<<"Cos dari Sudut:"<<list_sudut[i]<<"="<<cosinus(list_sudut[i])<<std::endl;
+    }
+    return 0;
+}
