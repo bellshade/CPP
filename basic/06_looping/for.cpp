1ce8615ecbac9667af137e2570834487b4e05a21
= <muhbintang650@gmail.com>
2023-12-02 15:13:35 +0800

    feat: menambahkan contoh perulangan

---
+++ basic/06_looping/for.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    // Membuat perulangan for
+    /*
+        perulangan for :
+        perulangan yang kondisi nya dapat ditulis dalam satu baris
+    */
+
+    // int i = variabel (nilai awal)
+    // i <= 5 = kondisi yang harus terpenuhi
+    // i++ = increment (menambahkan nilai tiap perulangan)
+
+    for (int i = 1; i <= 5; i++) // ditulis dalam satu baris
+    {
+        std::cout << "Hello World\n"; // menampilkan data hello world sebanyal 5x
+    }
+
+    return 0;
+}
