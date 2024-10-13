656e1e5a4987ef14698771edaf12e9fe23238347
yusuf601 <yusufmuhyusuh@gmail.com>
2024-10-13 12:27:37 +0700

    fix: Insertion sort

--- backtracking/subarray_sum.cpp
+++ backtracking/subarray_sum.cpp
@@ -10,62 +10,59 @@
  * array saja.
  */
 
-#include <iostream>
 #include <cassert>
+#include <cstdint>
+#include <iostream>
 #include <unordered_map>
 #include <vector>
 
 /**
  * @brief algoritma backtracking
  * @namespace backtracking
- * 
+ *
  */
 namespace backtracking {
-    namespace subarray_sum{
-        /**
-         * @brief fungsi utama untuk implementasi dari subarray sum
-         * @param sum adalah yang diperlukan dari setiap subarray
-         * @param in_arr adalh input dari array
-         * @return jumlah dari angka set
-         */
-        uint64_t subarray_sum(int64_t sum, const std::vector<int64_t> &in_arr){
-            int64_t nelement = in_arr.size();
-            int64_t count_of_subset = 0;
-            int64_t current_sum = 0;
-            std::unordered_map<int64_t, int64_t>
-                sumarray;
-            
-            for (int64_t i = 0; i < nelement; i++){
-                current_sum += in_arr[i];
-                if (current_sum == sum){
-                    count_of_subset++;
-                }
+namespace subarray_sum {
+/**
+ * @brief fungsi utama untuk implementasi dari subarray sum
+ * @param sum adalah yang diperlukan dari setiap subarray
+ * @param in_arr adalh input dari array
+ * @return jumlah dari angka set
+ */
+uint64_t subarray_sum(int64_t sum, const std::vector<int64_t> &in_arr) {
+  int64_t nelement = in_arr.size();
+  int64_t count_of_subset = 0;
+  int64_t current_sum = 0;
+  std::unordered_map<int64_t, int64_t> sumarray;
 
-                // jika seandainya current_sum lebih besar
-                // dari jumlah yang dibutuhkan
-                if (sumarray.find(current_sum - sum) != sumarray.end()){
-                    count_of_subset += (sumarray[current_sum - sum]);
-                }
-                sumarray[current_sum]++;
-            }
-            return count_of_subset;
-        }
+  for (int64_t i = 0; i < nelement; i++) {
+    current_sum += in_arr[i];
+    if (current_sum == sum) {
+      count_of_subset++;
     }
+
+    // jika seandainya current_sum lebih besar
+    // dari jumlah yang dibutuhkan
+    if (sumarray.find(current_sum - sum) != sumarray.end()) {
+      count_of_subset += (sumarray[current_sum - sum]);
+    }
+    sumarray[current_sum]++;
+  }
+  return count_of_subset;
+}
+} // namespace subarray_sum
+} // namespace backtracking
+
+static void testing() {
+  std::cout << "test pertama ";
+  std::vector<int64_t> array1 = {-7, -3, -2, 5, 8};
+  assert(backtracking::subarray_sum::subarray_sum(0, array1) == 1);
+
+  std::cout << "berhasil!" << std::endl;
 }
 
-static void testing(){
-    std::cout<<"test pertama ";
-    std::vector<int64_t> array1 = {-7, -3, -2, 5, 8};
-    assert(
-        backtracking::subarray_sum::subarray_sum(0, array1) == 1
-    );
+int main() {
+  testing();
 
-    std::cout<<"berhasil!"<<std::endl;
-}
-
-
-int main(){
-    testing();
-
-    return 0;
+  return 0;
 }
