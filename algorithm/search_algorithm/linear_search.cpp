c22079f6faf3892252c647349da80c6cab0a48ba
slowy07 <slowy.arfy@proton.me>
2024-10-20 14:42:21 +0700

    fix: mengganti builtin function getline
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- algorithm/search_algorithm/linear_search.cpp
+++ algorithm/search_algorithm/linear_search.cpp
@@ -1,170 +1,169 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include<array>
-#include <stdlib.h>
-using namespace std;
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+void clearScreen() {
+#ifdef _WIN32
+  std::system("cls");
+#else
+  std::system("clear");
+#endif // DEBUG
+}
+
 /*sumber crud dan saya memodifikasi sedikit
 https://gist.github.com/nihitx/1f56c129b35c992d35ba9c11647d2721
-karena program ini fokus ke implementasi linear search bukan membuat crud maka 
+karena program ini fokus ke implementasi linear search bukan membuat crud maka
 penjelesan nya hanya fokus ke linear search
 */
-bool SearchName(vector<string>& nama, string &DataNama){
-    // Menampilkan pesan untuk meminta pengguna memasukkan nama penulis yang ingin dicari.
-    cout << "Masukkan nama penulis yang anda ingin cari ";
-    
-    // Mengambil input dari pengguna dan menyimpannya dalam variabel DataNama.
-    getline(cin, DataNama);
-
-    // Looping melalui vektor 'nama' untuk mencari kecocokan dengan input pengguna.
-    for(int i = 0; i < nama.size(); i++){
-        // Jika nama penulis yang dicari ditemukan di vektor, kembalikan nilai true.
-        if(DataNama == nama[i]){
-            return true;
-        }
+bool SearchName(std::vector<std::string> &nama, std::string &DataNama) {
+  // looping melalui vektor `nama` untuk mencari kecocokan dengan input pengguna
+  for (const auto &name : nama) {
+    if (DataNama == name) {
+      return true;
     }
-    
-    // Jika looping selesai tanpa menemukan kecocokan, kembalikan nilai false.
-    return false;
+  }
+  return false;
 }
-bool SearchBook(vector<string>& buku, string &DataBuku){
-    // Menampilkan pesan untuk meminta pengguna memasukkan nama buku yang ingin dicari.
-    cout << "Masukkan nama buku yang anda ingin cari ";
-    getline(cin,DataBuku);
-    // Looping melalui vektor 'buku' untuk mencari buku berdasarkan input pengguna.
-    for(int i = 0; i < buku.size(); i++){
-        // Jika buku yang dicari ditemukan di vektor, kembalikan nilai true.
-        if(DataBuku == buku[i]){
-            return true;
-        }
+
+bool SearchBook(const std::vector<std::string> &buku,
+                const std::string &DataBuku) {
+  // looping melalui vektor `buku` untuk mencari buku berdasarkan input
+  for (const auto &book : buku) {
+    if (DataBuku == book) {
+      return true;
     }
-    
-    // Jika looping selesai tanpa menemukan kecocokan, kembalikan nilai false.
-    return false;
-}
-bool LinearSearch(vector<string>& bookarray, string &data){
-    // Menampilkan pesan untuk meminta pengguna memasukkan buku yang ingin dicari.
-    cout << "Masukkan buku yang anda ingin cari ";
-    
-    // Mengambil input dari pengguna dan menyimpannya dalam variabel 'data'.
-    getline(cin, data);
-    
-    // Looping melalui vektor 'bookarray' untuk mencari buku berdasarkan input pengguna.
-    for(int i = 0; i < bookarray.size(); i++){
-        // Jika buku yang dicari ditemukan di vektor, kembalikan nilai true.
-        if(data == bookarray[i]){
-            return true;
-        }
-    }  
-    
-    // Jika looping selesai tanpa menemukan kecocokan, kembalikan nilai false.
-    return false;
+  }
+  return false;
 }
 
-int main(){
-    cout << "**  welcome to the database **" << endl;
-    char choice;
-    int pilihan;
-    string data;
-    string DataNama;
-    string DataBuku;
-    string bookname;
-    string removebook;
-    int countnum;
-    string authorname;
-    string fullbook;
-    vector<string> bookarray;
-    vector<string> nama;
-    vector<string> buku;
-    do{
-        system("clear");
-        cout << " a. Tambah buku" << endl;
-        cout << " b. Hapus buku" << endl;
-        cout << " c. Kosongkan database" << endl;
-        cout << " d. Tampilkan jumlah buku" << endl;
-        cout << " e. Tampilkan semua buku" << endl;
-        cout << " f. cari buku " << endl;
-        cout << " g. keluar" << endl;
-        cout << "Masukkan pilihan anda: ";
-        cin >> choice;
-        if(choice == 'g' ) {
-            printf("Mengakhiri program...");
-        }
-        else{
-            if(choice == 'a'){
-                cout << "Masukkan judul buku: " ;
-                cin >> bookname;
-                buku.push_back(bookname);
-                cout << "Masukkan nama penulis :";
-                cin >> authorname;
-                nama.push_back(authorname);
-                fullbook = bookname + " " + authorname;
-                bookarray.push_back(fullbook);
-                cout << "Buku berhasil di tambahkan" << endl;
-            }else if(choice =='b'){
-                cout << "Masukkan judul buku yang ingin dihapus: " ;
-                cin >> removebook;
-                for(int i = 0; i < bookarray.size() ; i++){
-                    if (bookarray[i].find(removebook) != std::string::npos) {
-                        bookarray.erase (bookarray.begin()+i);
-                        break;
-                    }
-                }
-                cout << " " << endl;
-                cout << "Buku berhasil di hapus" << endl;
-                cout << " " << endl;
-            }else if(choice == 'c'){
-                bookarray.clear();
-                cout << "Database telah dikosongkan"  << endl;
-                cout << " " << endl;
-            }else if(choice == 'd'){
-                countnum = bookarray.size();
-                cout << "Jumlah buku dalam database:" << countnum << endl;
-            }else if(choice == 'e'){
-                cout << "Daftar buku dalam database " << endl;
-                for(int i = 0; i < bookarray.size() ; i++){
-                    cout << i + 1 << " " << bookarray[i] <<  " " << endl; ;
-                }
-                cout << " " << endl;
-            }else if(choice == 'f'){
-                cout << "1.cari buku berdasarkan nama penulis \n";
-                cout << "2.cari buku berdasarkan nama buku \n";
-                cout << "3.cari buku berdasarkan \n";
-                cout << "Masukkan pilihan anda: ";
-                cin >> pilihan;
-                cin.ignore();
-                switch(pilihan){
-                    case 1:
-                        if(SearchName(nama,DataNama)){
-                            cout << "data penulis ditemukan! " << endl;
-                            break;
-                        }else{
-                            cout << "Data penulis tidak di temukan " << endl;
-                            break;
-                        }
-                    case 2:
-                        if(SearchBook(buku,DataBuku)){
-                            cout << "Data Buku ditemukan! " << endl;
-                            break;
-                        }else{
-                            cout << "Data buku tidak ditemukan " << endl;
-                            break;
-                        }
-                    case 3:
-                        if(LinearSearch(bookarray,data)){
-                            cout << "Data Buku ditemukan! " << endl;
-                            break;
-                        }else{
-                            cout << "Data buku tidak ditemukan! " << endl;
-                            break;
-                        }
-                    default:
-                        cout << "Invalid input " << endl;         
-                        break;   
-                }
-             }
-        }
-    }while(choice != 'g');
+bool LinearSearch(const std::vector<std::string> &bookarray,
+                  const std::string &data) {
+  // looping melalui vektor `bokarray` untuk searching buku berdasarkan input
+  for (const auto &book : bookarray) {
+    if (data == book) {
+      return true;
+    }
+  }
+  return false;
+}
 
-    return 0;
+int main() {
+  char choice;
+  int pilihan;
+  std::string data, DataNama, DataBuku, bookname, removebook, authorname,
+      fullbook;
+  std::vector<std::string> bookarray, nama, buku;
+  int countnum;
+
+  do {
+    clearScreen();
+    std::cout << "a. tambah buku" << std::endl;
+    std::cout << "b. hapus buku" << std::endl;
+    std::cout << "c. kosongkan database" << std::endl;
+    std::cout << "d. tampillkan jumlah buku" << std::endl;
+    std::cout << "e. tampilkan semua buku" << std::endl;
+    std::cout << "f. cari buku" << std::endl;
+    std::cout << "g. keluar" << std::endl;
+    std::cin >> choice;
+
+    if (choice == 'g') {
+      std::cout << "close program" << std::endl;
+    } else {
+      switch (choice) {
+      case 'a':
+        std::cout << "masukkan judul buku: ";
+        std::cin >> bookname;
+        buku.push_back(bookname);
+
+        std::cout << "masukkan nama penulis: ";
+        std::cin >> authorname;
+        nama.push_back(authorname);
+
+        fullbook = bookname + " " + authorname;
+        bookarray.push_back(fullbook);
+        std::cout << "buku berhasil ditambah" << std::endl;
+        break;
+
+      case 'b':
+        std::cout << "masukkan judul buku yang ingin dihapus: ";
+        std::cin >> removebook;
+        for (auto it = bookarray.begin(); it != bookarray.end(); ++it) {
+          if (it->find(removebook) != std::string::npos) {
+            bookarray.erase(it);
+            std::cout << "buku berhasil dihapus" << std::endl;
+            break;
+          }
+        }
+        break;
+      case 'c':
+        bookarray.clear();
+        std::cout << "database telah dikosongkan" << std::endl;
+        break;
+
+      case 'd':
+        countnum = bookarray.size();
+        std::cout << "jumlah buku dalam database: " << countnum << std::endl;
+        break;
+
+      case 'e':
+        std::cout << "daftar buku dalam database: " << std::endl;
+        for (size_t i = 0; i < bookarray.size(); ++i) {
+          std::cout << i + 1 << ". " << bookarray[i] << std::endl;
+        }
+        break;
+
+      case 'f':
+        std::cout << "1. cari buku berdasarkan nama penulis" << std::endl;
+        std::cout << "2. cari buku berdasarkan nama buku" << std::endl;
+        std::cout << "3. cari buku berdasarkan judul dan penulis" << std::endl;
+        std::cin >> pilihan;
+
+        switch (pilihan) {
+        case 1:
+          std::cout << "masukkan nama penulis: ";
+          std::cin >> DataNama;
+          if (SearchName(nama, DataNama)) {
+            std::cout << "data penulis ditemukan" << std::endl;
+          } else {
+            std::cout << "data penulis tidak ditemukan" << std::endl;
+          }
+          break;
+
+        case 2:
+          std::cout << "masukkan nama buku: ";
+          std::cin >> DataBuku;
+          if (SearchBook(buku, DataBuku)) {
+            std::cout << "data buku ditemukan" << std::endl;
+          } else {
+            std::cout << "data buku tidak ditemukan" << std::endl;
+          }
+          break;
+
+        case 3:
+          std::cout << "masukkan judul atau penulis buku: ";
+          std::cin >> data;
+          if (LinearSearch(bookarray, data)) {
+            std::cout << "data buku ditemukan" << std::endl;
+          } else {
+            std::cout << "data buku tidak ditemukan" << std::endl;
+          }
+          break;
+
+        default:
+          std::cout << "pilihan tidak valid" << std::endl;
+          break;
+        }
+        break;
+
+      default:
+        std::cout << "pilihan tidak valid" << std::endl;
+        break;
+      }
+    }
+  } while (choice != 'g');
+  return 0;
 }
