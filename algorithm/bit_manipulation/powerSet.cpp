c8402ac868d6132e016c2a75707af5764e0be88e
Muh Yusuf <79491810+yusuf601@users.noreply.github.com>
2025-10-02 14:10:17 +0800

    Feat/power set
    # Perubahan yang diberikan
    ### Menambahkan Program Power Set
    
    `PowerSet` adalah algoritma untuk mendapatkan atau mencetak semua **sub-himpunan** dari sebuah himpunan.  
    
    Contoh:  
    Jika himpunan `{1, 2, 3}`, maka akan menghasilkan output:
    ```bash
    { }{ 1 }{ 2 }{ 1 2 }{ 3 }{ 1 3 }{ 2 3 }{ 1 2 3 }
    ```
    # Checklist:
    ##### Umum:
    - [x] Saya menambah algoritma terbaru.
    - [x] Saya memperbaiki dokumentasi.
    - [x] Saya menambah dokumentasi.
    
    ##### Contributor Requirements (Syarat Kontributor) dan Lain-Lain:
     - [x] Saya sudah membaca (I have read) [CONTRIBUTING](https://github.com/bellshade/CPP/blob/main/CONTRIBUTING.md) dan sudah menyetujui semua syarat.
     - [x] Saya telah menambahkan komentar kode yang memberikan penjelasan maksud dari kode yang saya buat.
     - [x] Saya menggunakan bahasa Indonesia untuk memberikan penjelasan dari kode yang saya buat.
    
    # Environment
    Saya menggunakan (I'm using):
    
    - ``OS`` = `linux` 
    - ``g++`` = `15.2.1` 
    
    
    
    # Link Issues
    
    Issues : # 
    
    
    
    This Commit License
    https://github.com/bellshade/CPP/blob/main/license
    
    Co-authored-by: bellshadebot <bellshade07@gmail.com>

---
+++ algorithm/bit_manipulation/powerSet.cpp
@@ -0,0 +1,48 @@
+/**
+ * @brief program untuk memsimulasikan algoritma powerSet yang digunakan
+ * untuk mendapatkan semua sub himpunan dari semua himpunan.
+ * 
+ * @author yusuf
+ * @date 9 september 2025
+ */
+#include <iostream>
+#include <vector>
+/**
+ * @brief powerSet adalah salah satu algoritma untuk mendapatkan semua subset
+ * sebuah himpunan(set),ini dibutuhkan ketika menghadapi masalah/problem seperti
+ * perfect sum.algoritma ini memiliki 2 metode untuk mengaplikasinnya yaitu
+ * backtracking dan bitmasking,Contoh dibawah memakai bitmasking
+ * 
+ * jika kita punya n element,maka banyak subset adalah 2^n.
+ * bilangan biner dengan n bit dapat membentuk tepat 2^n kombinasi
+ * misal n = 3,maka total subset adalah 2^3 = 8
+ * semua bilangan biner 3 bit mulai 000(0) sampai 111(7) bisa dipakai untuk
+ * mewakili subset
+ * 
+ * aturannya:
+ *  bit ke-j = 1 ->element ke -j masuk subset
+ *  bit ke-j = 0 ->element ke j tidak masuk subset
+ * @details Time complexity O(n^2),Space Complexity O(n)
+ */
+void PowerSet(){
+    std::vector<int>nums = {1,2,3};
+    int n = nums.size();
+    for(int mask = 0;mask < (1 << n);mask++){ //iterasi sampai 2^n(banyak subset)
+        std::vector<int>subset;
+        for(int i = 0;i < n;i++){ 
+            if(mask & (1 << i)){ // cek apakah bit ke i menyala
+                subset.push_back(nums[i]);
+            }
+        }
+        std::cout << "{ ";
+        for(auto x: subset){
+            std::cout << x << " ";
+        }
+        std::cout << "}";
+    }
+}
+int main(){
+    PowerSet();
+    std::cin.get();
+    return 0;
+}
